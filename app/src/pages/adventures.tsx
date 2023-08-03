730beaab4e3371987cf6ef4c4d620ea69bd90796
Jayson Acosta <36345453+jaysonacosta@users.noreply.github.com>
2023-08-03 16:16:19 -0400

    Redirecting after login

---
+++ app/src/pages/adventures.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+export default function Adventures() {
+  return (
+    <>
+      <Head>
+        <title>AIVenture | Adventures</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="">
+        <h1 className="text-xl font-extrabold tracking-tight text-gray-800">
+          My Adventures
+        </h1>
+      </main>
+    </>
+  );
+}
