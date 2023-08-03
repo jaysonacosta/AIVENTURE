3fec55398b2b9f94afa16599f995c6b22bce3548
Jayson Acosta <36345453+jaysonacosta@users.noreply.github.com>
2023-08-03 16:58:11 -0400

    Rearranged and added elements

--- app/src/pages/adventures.tsx
+++ app/src/pages/adventures.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+import { signOut } from "next-auth/react";
 
 export default function Adventures() {
   const { status, data: session } = useSession();
@@ -20,11 +21,25 @@ export default function Adventures() {
   } else if (status === "authenticated") {
     content = (
       <>
-        {" "}
-        <h1 className="text-xl font-extrabold tracking-tight text-gray-800">{`Hi, ${session.user.name}`}</h1>
-        <h1 className="text-xl font-extrabold tracking-tight text-gray-800">
-          My Adventures
-        </h1>
+        <div className="flex items-center justify-between bg-gray-100/70 p-5">
+          <p className="text-xl font-extrabold tracking-tight text-gray-800">{`Hi, ${session.user.name}`}</p>
+          <button
+            className="rounded-full bg-slate-300 px-10 py-3 font-bold text-gray-800 no-underline transition hover:bg-slate-300/80"
+            onClick={() => void signOut()}
+          >
+            Sign out
+          </button>
+        </div>
+        <div className="p-5">
+          <div className="flex items-center justify-between">
+            <h1 className="text-3xl font-extrabold tracking-tight text-gray-800">
+              My Adventures
+            </h1>
+            <button className="rounded-full bg-slate-300 px-10 py-3 font-bold text-gray-800 no-underline transition hover:bg-slate-300/80">
+              New Adventure
+            </button>
+          </div>
+        </div>
       </>
     );
   }
