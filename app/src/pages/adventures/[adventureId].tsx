229c77351932b2fdac4a2429a1bab09a0d95143b
Jayson Acosta <36345453+jaysonacosta@users.noreply.github.com>
2023-08-08 01:44:24 +0200

    Created textarea and input for adventure text

--- app/src/pages/adventures/[adventureId].tsx
+++ app/src/pages/adventures/[adventureId].tsx
@@ -34,8 +34,29 @@ export default function AdventureId() {
           {adventure.characterName}
         </h2>
         <h3 className="text-lg font-bold tracking-tight text-gray-400">
-          {adventure.createdAt.toISOString()}
+          {adventure.createdAt.toLocaleDateString("en-US", {
+            month: "long",
+            day: "numeric",
+            year: "numeric",
+          })}
         </h3>
+        <div className="mt-5 flex flex-col gap-5">
+          <p className="text-xl font-bold text-gray-800">Dungeon Master</p>
+          <textarea
+            className="w-full resize-none bg-slate-100 p-2 shadow"
+            readOnly={true}
+          ></textarea>
+          <p className="text-xl font-bold text-gray-800">What will you do?</p>
+          <input type="text" className=" h-12 w-full bg-slate-100 p-2 shadow" />
+          <div className="flex gap-5">
+            <button className="rounded bg-green-500 p-4 font-semibold text-white hover:bg-green-400">
+              Submit
+            </button>
+            <button className="rounded bg-blue-500 p-4 font-semibold text-white hover:bg-blue-400">
+              Give me some options
+            </button>
+          </div>
+        </div>
       </div>
     );
   }
